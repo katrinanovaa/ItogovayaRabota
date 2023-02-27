50d1977701bcbda76f697dc920ba1ae0b6cc5296
Катерина Андреевна <katrina_nova@mail.ru>
2023-02-27 22:51:13 +0500

    код написан, нужна отладка, есть косяки

--- Program_Itog/Program.cs
+++ Program_Itog/Program.cs
@@ -10,3 +10,41 @@
 {"1234", "1567", "-2", "computer science"}  -> {"-2"}
 {"Russia", "Denmark", "Kazan"}  -> {}*/
 
+using System.Text;     // используем using для работы со строками
+
+void GetSecondNumbers(string[] array1, string[] array2)
+{
+    int count = 0;
+    for(int i = 0; i < array1.Length; i++)
+    {
+        if(array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+    if (count == 0)
+    {
+            Console.WriteLine("Нет слов меньше трех символов");
+    }
+}
+void PrintArray(string[] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+            Console.Write("{0,8:F2}", $"{inArray[i]}\t "); 
+    }
+}
+
+Console.Clear();
+Console.WriteLine("Введите слова или числа через запятую:");
+
+StringBuilder readWords = new StringBuilder(Console.ReadLine());
+string[] array = readWords.Replace(",", " ").ToString().Split(" ",StringSplitOptions.RemoveEmptyEntries);   //заменяем "," на пробелы, а потом пробелы являются разделителями в строке
+//Console.WriteLine(string.Join(", ",array));
+string[] array2 = new string [array.Length];
+
+GetSecondNumbers(array, array2);
+//Console.WriteLine("Из введенных данных, мы получили массив из строк, которые меньше или равны 3 символам");
+PrintArray(array2);
+
